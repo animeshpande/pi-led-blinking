8a3c8ed0af24801277c3b8378cac768f5458fb1f
Animesh Pande <animesh.pande@carleton.ca>
2018-09-16 19:38:22 -0600

    multiple flashing LEDs

--- blinking.py
+++ blinking.py
@@ -1,6 +1,20 @@
 #! /usr/bin/python
 from gpiozero import LED
+from time import sleep
 
-led = LED(15)
+count = 0
+led1 = LED(15)
+led2 = LED(14)
+led3 = LED(18)
 
-led.on()
+def toggle_led(led):
+    sleep(1)
+    led.on()
+    sleep(0.5)
+    led.off
+
+while count < 50:
+    toggle_led(led1)
+    toggle_led(led2)
+    toggle_led(led3)
+    count += 1
