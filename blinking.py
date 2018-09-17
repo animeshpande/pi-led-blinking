c86b4315ad4ed4a60620d0757fcbadf079a8a960
Animesh Pande <animeshpande2810@gmail.com>
2018-09-16 19:52:10 -0600

    3 LED wave loop

--- blinking.py
+++ blinking.py
@@ -7,14 +7,16 @@ led1 = LED(15)
 led2 = LED(14)
 led3 = LED(18)
 
-def toggle_led(led):
-    sleep(1)
-    led.on()
-    sleep(0.5)
-    led.off
+def toggle_led(led1,led2):
+    led1.on()
+    sleep(0.005)
+    led2.on()
+    sleep(0.005)
+    led1.off()
+    sleep(0.1)
 
-while count < 50:
-    toggle_led(led1)
-    toggle_led(led2)
-    toggle_led(led3)
+while True: #count < 50:
+    toggle_led(led1,led2)
+    toggle_led(led2,led3)
+    toggle_led(led3,led1    )
     count += 1
